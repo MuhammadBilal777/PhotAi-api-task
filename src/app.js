2713f73bf953e792a33b8578549cbf4048752a87
MuhammadBilal777 <muhammadbilalmb010@gmail.com>
2024-07-05 23:58:45 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
-import { errorHandler, rateLimiter, swagger } from "#middlewares";
+import { errorHandler, swagger } from "#middlewares";
 import swaggerUi from "swagger-ui-express";
 import routes from "./routes/image-tools.route.js";
 const app = express();
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
-app.use(rateLimiter);
 
 // Serve Swagger UI at /api-docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
@@ -22,7 +21,7 @@ app.use("/api/v1", routes);
 // Express No Route middleware
 app.use("*", (req, res, next) => {
   res.status(404).json({
-    message: `No route ${req.originalUrl} available in this server`,
+    message: No route ${req.originalUrl} available in this server,
     success: false,
   });
   next();
