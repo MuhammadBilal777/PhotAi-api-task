280a7e7bd0fc7b32aed4ff83d582dae03dc86dc5
MuhammadBilal777 <muhammadbilalmb010@gmail.com>
2024-07-06 00:06:28 +0500

    Update rateLimiter.js

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,12 +1,12 @@
-import {ApiError} from '#utils';
-import rateLimit from 'express-rate-limit';
+import { ApiError } from "#utils";
+import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000,
-  max: 50,
+  max: 5,
   handler: (req, res, next) => {
-    next(new ApiError('Too many requests, please try again later.', 429));
-  }
+    next(new ApiError("Too many requests, please try again later.", 429));
+  },
 });
 
 export default limiter;
